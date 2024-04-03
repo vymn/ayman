c96e2788c87e1e4d56ed9e444bc1356171b5b5f6
vymn mohvmd <78211839+vymn@users.noreply.github.com>
2024-04-03 22:49:32 +0400

    trying to fix workflow issues

--- components/main/About.jsx
+++ components/main/About.jsx
@@ -1,7 +1,7 @@
 "use client";
 
 import React from "react";
-import Tilt from "react-tilt";
+import { Tilt } from "react-tilt";
 import { motion } from "framer-motion";
 import { styles } from "../../styles";
 import { services } from "../../constants/constants/index";
